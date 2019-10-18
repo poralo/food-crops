fac3c0148547cd34f4b89c4b99bea2f3a9f37ce1
Jules Warot <jules.warot@mines-ales.org>
2019-10-18 17:58:24 +0200

    Mise en place des fonctions de la classe FoodCropFactory

---
+++ FoodCropsFactory.py
@@ -0,0 +1,50 @@
+from Unit import Unit
+
+class FoodCropFactory():
+
+    def __init__(self):
+        pass
+
+    def createVolume(self,id: int):
+        # Return Unit
+        pass
+
+    def createPrice(self, id: int):
+        # Return Unit
+        pass
+
+    def createWeight(self, id: int, weight: float):
+        # Return Unit
+        pass
+
+    def createSurface(self, id: int):
+        # Return Unit
+        pass
+
+    def createCount(self, id: int, what: str):
+        # Return Unit
+        pass
+
+    def createRatio(self, id: int):
+        # Return Unit
+        pass
+
+    def createUnitRatio(self, id: int, unit1: Unit, unit2: Unit):
+        # Return Unit
+        pass
+
+    def createCommodity(self, group: CommodityGroup, id: int, name: str):
+        # Return Unit
+        pass
+
+    def createIndicator(self, id: int, frequency: int, freqDesc: str, geogLocation: str, indicatorGroup: IndicatorGroup, unit:Unit):
+        # Return Indicator
+        pass
+
+    def createMesurement(self, id: int, description: str):
+        # Return MeasurementType
+        pass
+
+    def createMeasurement(self, id: int, year: int, value: float, timeperiodId: int, timeperiodDesc: str, type: MeasurementType, commodity: Commodity, indicator: Indicator):
+        # Return Measurement
+        pass
