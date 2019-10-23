934929d550834e1c5647b826a0a5c15ec7da025f
Jules Warot <jules.warot@mines-ales.org>
2019-10-23 14:48:19 +0200

    Modification de la syntaxe pour dire quel type est retourné dans la classe FoodCropsFactory et imports nécessaires

--- FoodCropsFactory.py
+++ FoodCropsFactory.py
@@ -1,4 +1,10 @@
 from Unit import Unit
+from CommodityGroup import CommodityGroup
+from IndicatorGroup import IndicatorGroup
+from Indicator import Indicator
+from MeasurementType import MeasurementType
+from Commodity import Commodity
+from Measurement import Measurement
 
 
 class FoodCropFactory():
@@ -9,44 +15,34 @@ class FoodCropFactory():
     def createVolume(self, id: int) -> Unit:
         pass
 
-    def createPrice(self, id: int):
-        # Return Unit
+    def createPrice(self, id: int) -> Unit:
         pass
 
-    def createWeight(self, id: int, weight: float):
-        # Return Unit
+    def createWeight(self, id: int, weight: float) -> Unit:
         pass
 
-    def createSurface(self, id: int):
-        # Return Unit
+    def createSurface(self, id: int) -> Unit:
         pass
 
-    def createCount(self, id: int, what: str):
-        # Return Unit
+    def createCount(self, id: int, what: str) -> Unit:
         pass
 
-    def createRatio(self, id: int):
-        # Return Unit
+    def createRatio(self, id: int) -> Unit:
         pass
 
-    def createUnitRatio(self, id: int, unit1: Unit, unit2: Unit):
-        # Return Unit
+    def createUnitRatio(self, id: int, unit1: Unit, unit2: Unit) -> Unit:
         pass
 
-    def createCommodity(self, group: CommodityGroup, id: int, name: str):
-        # Return Unit
+    def createCommodity(self, group: CommodityGroup, id: int, name: str) -> Unit:
         pass
 
     def createIndicator(self, id: int, frequency: int, freqDesc: str, geogLocation: str, indicatorGroup: IndicatorGroup,
-                        unit: Unit):
-        # Return Indicator
+                        unit: Unit) -> Indicator:
         pass
 
-    def createMesurement(self, id: int, description: str):
-        # Return MeasurementType
+    def createMesurement(self, id: int, description: str) -> MeasurementType:
         pass
 
     def createMeasurement(self, id: int, year: int, value: float, timeperiodId: int, timeperiodDesc: str,
-                          type: MeasurementType, commodity: Commodity, indicator: Indicator):
-        # Return Measurement
+                          type: MeasurementType, commodity: Commodity, indicator: Indicator) -> Measurement:
         pass
