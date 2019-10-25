dfa256aaa5eda98b4ebad3886ab5fbbd89d03c55
magali0411 <magali.arhainx@gmail.com>
2019-10-25 17:35:53 +0200

    m

--- script1.py
+++ script1.py
@@ -13,20 +13,41 @@ L06 = []
 L07 = []
 L08 = []
 L09 = []
-L010 = []
+L10 = []
+L11 = []
+L12 = []
+L13 = []
+L14 = []
+L15 = []
+L16 = []
+L17 = []
+L18 = []
+L19 = []
 
-for line in fd:
-    data = line.split(",")
-    L01.append(data[0])
-    L02.append(data[1])
-    L03.append(data[2])
-    L04.append(data[3])
-    L05.append(data[4])
-    L06.append(data[5])
-    L07.append(data[6])
-    L08.append(data[7])
-    L09.append(data[8])
+def open():
+    for line in fd:
+        data = line.split(",")
+        L01.append(data[0])
+        L02.append(data[1])
+        L03.append(data[2])
+        L04.append(data[3])
+        L05.append(data[4])
+        L06.append(data[5])
+        L07.append(data[6])
+        L09.append(data[8])
+        L10.append(data[9])
+        L11.append(data[10])
+        L12.append(data[11])
+        L13.append(data[12])
+        L14.append(data[13])
+        L15.append(data[14])
+        L16.append(data[15])
+        L17.append(data[16])
+        L18.append(data[17])
+        L19.append(data[18])
+    return L01
+    return L02
 
-print(L09)
+print(open())
+
 fd.close()
-
