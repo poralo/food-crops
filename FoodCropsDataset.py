9cc93926ea6ceebfc2eaf29114d844bdd778dd4e
magali0411 <magali.arhainx@gmail.com>
2019-11-17 20:05:04 +0100

    nouveau lancement script

--- FoodCropsDataset.py
+++ FoodCropsDataset.py
@@ -2,7 +2,7 @@ from FoodCropFactory import FoodCropFactory
 from CommodityGroup import CommodityGroup
 from IndicatorGroup import IndicatorGroup
 from Unit import Unit
-
+import argparse
 import pandas
 
 class FoodCropsDataset:
@@ -122,12 +122,43 @@ class FoodCropsDataset:
 
         return measurementList
 
+
+
+
 if __name__ == "__main__":
     f = FoodCropFactory()
     fcd = FoodCropsDataset(f)
     fcd.load("FeedGrainsLight.csv")
 
     # CommodityGroup.SORGHUM, IndicatorGroup.PRICES
-    liste = fcd.findMeasurements()
-    print(liste)
-    print(liste[0].describe())
+    
+    # Arguments à donner au script
+    parser=argparse.ArgumentParser(description='Tri à appliquer sur les données food-crops')
+
+    parser.add_argument("-cg","--CommodityGroup", type=str, nargs='?')
+    parser.add_argument("-ig","--IndicatorGroup", type=str, nargs='?')
+    parser.add_argument("-gl","--geographicalLocation", type=str, nargs='?')
+    parser.add_argument("-u","--Unit", type=str, nargs='?')
+
+    args, unknown = parser.parse_known_args() #gestion des arguments vide
+
+    cg = None
+    ig = None
+
+    if args.CommodityGroup :
+        try :
+            cg = CommodityGroup[args.CommodityGroup]
+        except :
+            cg = "Unknown CommodityGroup"
+
+    if args.IndicatorGroup :
+        try :
+            ig = IndicatorGroup[args.IndicatorGroup]
+        except :
+            ig = "Unknown IndicatorGroup"
+
+    # Affichage de la liste des mesures
+    liste = fcd.findMeasurements(cg,ig,args.geographicalLocation,args.Unit)
+
+    for n in liste :
+         print(n.describe())
